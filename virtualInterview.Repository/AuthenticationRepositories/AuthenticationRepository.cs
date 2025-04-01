d7b3b74784728cc74765c0fe9c18cb8579329e76
Anusha Kunchala <Anusha.K@deccansoft.net>
2025-04-01 10:28:50 +0530

    Admin Login changes

---
+++ virtualInterview.Repository/AuthenticationRepositories/AuthenticationRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using virtualInterview.EFCore;
+using virtualInterview.Models.Utility;
+
+namespace virtualInterview.Repository.AuthenticationRepositories
+{
+    public class AuthenticationRepository : IAuthenticationRepository
+    {
+        private readonly VirtualInterviewDbContext _context;
+        // Step 1: Implement constructor and inject database context
+        //Step 2:Implement Method to Authenticates an admin user and generates a JWT token upon successful login.
+        //Step 3:Generates a JWT token for an authenticated admin user.
+
+    }
+
+}
