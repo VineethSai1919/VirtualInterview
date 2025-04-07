4cbfce94ddc1610856882e769a9639a02053eb3c
Raveendra Tadikonda <raveendra.t@deccansoft.net>
2025-04-07 18:24:24 +0530

    feedback

--- virtualinterview.client/src/app/app-routing.module.ts
+++ virtualinterview.client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './Shared/authguard.service';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 const routes: Routes = [{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),canActivate:[AuthGuard] },
   { path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserModule), canActivate: [AuthGuard] },
   { path: 'login', component: UserLoginComponent },
+  { path: 'feedback/:scheduleid', component: FeedbackComponent },
   { path: '', component: UserLoginComponent },
 ];
 
