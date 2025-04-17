a95b98e89d75923cc9b420a3bdd30351b88f46b5
vineeth Sai <vineeth.p@deccansoft.net>
2025-04-17 17:42:29 +0530

    Changes

--- virtualInterview.Functions/QuestionFunctions/QuestionAvatarTriggerFn.cs
+++ virtualInterview.Functions/QuestionFunctions/QuestionAvatarTriggerFn.cs
@@ -1,25 +1,17 @@
-﻿using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using virtualInterview.Repository.ErrorLogsRepositories;
-using virtualInterview.Repository.QuestionRepositories;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 
-namespace virtualInterview.Functions.QuestionFunctions
+namespace virtualinterview.Functions.QuestionFunctions
 {
-    /// Azure Function triggered by a blob upload to process question avatars.
     public class QuestionAvatarTriggerFn
     {
         [FunctionName("QuestionAvatarTriggerFn")]
-        public void Run([BlobTrigger("samples-workitems/{name}", Connection = "")] Stream myBlob, string name, ILogger log)
+        public void Run([BlobTrigger("%StorageConfig:QuestionsContainerName%/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-
         }
     }
 }
--- virtualInterview.Functions/Startup.cs
+++ virtualInterview.Functions/Startup.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using virtualInterview.EFCore;
-using virtualInterview.Repository.ErrorLogsRepositories;
-using virtualInterview.Repository.QuestionRepositories;
-using virtualInterview.Repository.SkillsRepositories;
-using virtualInterview.Repository.WebJobRepositories;
-
-[assembly: FunctionsStartup(typeof(virtualInterview.Functions.Startup))]
-namespace virtualInterview.Functions
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: FunctionsStartup(typeof(virtualinterview.Functions.Startup))]
+namespace virtualinterview.Functions
 {
     public class Startup : FunctionsStartup
     {
@@ -18,16 +14,6 @@ namespace virtualInterview.Functions
         {
             FunctionsHostBuilderContext context = builder.GetContext();
             ApplicationRootPath = context.ApplicationRootPath;
-
-            string con = context.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<VirtualInterviewDbContext>(options =>
-            {
-                options.UseSqlServer(con, s => s.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
-            }, ServiceLifetime.Scoped);
-            builder.Services.AddTransient<IQuestionRepository, QuestionRepository>();
-            builder.Services.AddTransient<IWebJobRepository, WebJobRepository>();
-            builder.Services.AddTransient<IErrorLogs, ErrorLogs>();
-            builder.Services.AddTransient<ISkillsRepository, SkillsRepository>();
         }
     }
 }
