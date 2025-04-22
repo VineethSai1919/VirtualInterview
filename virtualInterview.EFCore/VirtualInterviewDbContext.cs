c7438d551073322c907e5b0279c8ffc99ea476f7
Anusha Kunchala <Anusha.K@deccansoft.net>
2025-04-22 10:02:49 +0530

    changes

--- virtualInterview.EFCore/VirtualInterviewDbContext.cs
+++ virtualInterview.EFCore/VirtualInterviewDbContext.cs
@@ -9,5 +9,9 @@ namespace virtualInterview.EFCore
 {
     public class VirtualInterviewDbContext:DbContext
     {
+        public VirtualInterviewDbContext(DbContextOptions<VirtualInterviewDbContext> options) : base(options)
+        {
+
+        }
     }
 }
